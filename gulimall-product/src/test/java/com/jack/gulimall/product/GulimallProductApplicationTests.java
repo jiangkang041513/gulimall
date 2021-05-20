f8baa753e9980f1fff9e330ac1bb1cb478d95a03
Jack <1332602795@qq.com>
2021-05-20 19:11:54 +0800

    5/20 -- 创建各个板块modle

---
+++ gulimall-product/src/test/java/com/jack/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
